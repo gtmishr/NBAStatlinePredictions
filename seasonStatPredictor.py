dcdfbe8b6ae9512892e22fd7600fe2dbd89bff55
Gautam Mishra <gautami.mishra@outlook.com>
2019-06-09 15:53:52 +1000

    Created a basic player class and a sample player (Of the G.O.A.T)
    Starting to learn more of Python, how to create classes and stuff. Some of it is pretty weird (def?) but we're all good so far. Next step is to make the loop programmatically create all of the players with more advanced stats from the spreadsheet.

--- seasonStatPredictor.py
+++ seasonStatPredictor.py
@@ -1,8 +1,23 @@
 import csv
 
+class Player() :
+    def __init__(self, name, age, team, ppg, rpg, apg):
+        self.name = name
+        self.age = age
+        self.team = team
+        self.ppg = ppg
+        self.rpg = rpg
+        self.apg = apg
+
+    def listStats(self):
+        print(self.name + " is " + self.age + " years old, and averaged " + self.ppg + " points per game, " + self.rpg + " rebounds per game and " + self.apg + " assists per game this season for the " + self.team + ".")
+
 with open('Seasons_Stats.csv') as csvFile:
     csvReader = csv.reader(csvFile, delimiter = ',')
     lineCount = 0
     for row in csvReader:
         print(f'\t{row[2]} played in {row[1]} for the {row[5]}')
         lineCount += 1
+
+lebronJames = Player("Lebron James", "34", "LAL", "27.4", "8.5", "8.3")
+lebronJames.listStats()
