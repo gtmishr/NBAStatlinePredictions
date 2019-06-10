f6015bd729309ea7fba6f54071035607b8364950
Gautam Mishra <gautami.mishra@outlook.com>
2019-06-10 20:22:30 +1000

    This doesn't work
    Tried to implement adding everything to arrays, and a method of getting around the fact that the first few years of stats don't contain rebounds. This doesn't work, and yeah, I know that this is probably bad practise, but I wanted to prove that I've been trying to work on this stuff. Alright Google? You can still hire me.

--- seasonStatPredictor.py
+++ seasonStatPredictor.py
@@ -1,23 +1,37 @@
 import csv
 
 class Player() :
-    def __init__(self, name, age, team, ppg, rpg, apg):
+    def __init__(self, name, age, year, team, points, rebounds, assists):
         self.name = name
         self.age = age
+        self.year = year
         self.team = team
-        self.ppg = ppg
-        self.rpg = rpg
-        self.apg = apg
+        self.points = points
+        self.rebonds = rebounds
+        self.assists = assists
 
     def listStats(self):
-        print(self.name + " is " + self.age + " years old, and averaged " + self.ppg + " points per game, " + self.rpg + " rebounds per game and " + self.apg + " assists per game this season for the " + self.team + ".")
+        print(self.name + " is " + self.age + " years old, and averaged " + self.points + " points per game, " + self.rebounds + " rebounds per game and " + self.assists + " assists per game this season for the " + self.team + ".")
 
 with open('Seasons_Stats.csv') as csvFile:
     csvReader = csv.reader(csvFile, delimiter = ',')
     lineCount = 0
+    playersArray = []
+    parsedRebounds = 0
+
     for row in csvReader:
-        print(f'\t{row[2]} played in {row[1]} for the {row[5]}')
+        print(f'\t{row[2]} played in {row[1]} for the {row[parsedRebounds]}')
+
+        if {row[47]} != int:
+            parsedRebounds = 0
+        else:
+            parsedRebounds = {row[47]}
+
+        playersArray.append(Player(f'{row[2]}', f'{row[4]}', f'{row[1]}', f'{row[5]}', f'{row[52]}', f'{row[48]}', parsedRebounds))
+
         lineCount += 1
 
-lebronJames = Player("Lebron James", "34", "LAL", "27.4", "8.5", "8.3")
-lebronJames.listStats()
+for i in len(playersArray):
+    playersArray[i].listStats()
+
+playersArray.append(Player("Lebron James", "34", "2019", "LAL", "27.4", "8.5", "8.3"))
