07bbc91a5ba1396a318b3639e647175f836a3906
Gautam Mishra <gautami.mishra@outlook.com>
2019-06-17 18:31:27 +1000

    Functions and exit
    Added functions to make code not too messy moving forward, as well as a basic interface to do what the user wants to do. Added exit functionality. Added the ability to list stats and predict stats between two players, but listing doesn't work for some reason. Now to do actual homework.

--- seasonStatPredictor.py
+++ seasonStatPredictor.py
@@ -1,21 +1,47 @@
 import csv
 from sklearn import tree
 
-class Player() :
-    def __init__(self, name, age, year, team, gamesPlayed, points, assists):
-        self.name = name
-        self.age = age
-        self.year = year
-        self.team = team
-        self.gamesPlayed = gamesPlayed
-        self.points = points
-        self.assists = assists
+def opening():
 
-    def listStats(self):
+    statsArray = []
+    playersNamesArray = []
 
-        print(self.name + " is " + self.age + " years old, played " + self.gamesPlayed + " games, and scored " + self.points + " points, and " + self.assists + " assists this season for the " + self.team + ".")
+    initialInput = input("Welcome! What would you like to do? ")
 
-def createPlayerObject(firstUserNameSearch, secondUserNameSearch):
+    if (initialInput.lower() == "List Stats".lower()) or (initialInput.lower() == "L".lower()) or (initialInput.lower() == "list".lower()):
+        listPlayerStats()
+    elif (initialInput.lower() == "2 Player Analysis".lower()) or (initialInput.lower() == "2P".lower()):
+        twoPlayerPrediction()
+    elif (initialInput.lower() == "Exit".lower()) or (initialInput.lower() == "E".lower()):
+        print("Thanks for checking me out!")
+        quit()
+
+def createPlayerObjects(row):
+    statsArray.append([(float(f'{row[52]}')/float(f'{row[6]}')), (float(f'{row[47]}')/float(f'{row[6]}')), (float(f'{row[46]}')/float(f'{row[6]}')), float(f'{row[36]}'), float(f'{row[39]}')])
+    playersNamesArray.append(f'{row[2]}')
+
+def listPlayerStats(): # This currently doesn't work, but we'll get to it. I have to do actual homework now anyways.
+
+        with open('Seasons_Stats.csv') as csvFile:
+
+            csvReader = csv.reader(csvFile, delimiter = ',')
+            lineCount = 0
+            parsedRebounds = 0
+
+            for row in csvReader:
+
+                createPlayerObjects(lineCount)
+                lineCount += 1
+
+                print(statsArray[row])
+                print(playersNamesArray[row])
+
+            opening()
+
+def twoPlayerPrediction():
+
+    firstUserNameSearch = input("Enter a player's full name: ")
+    secondUserNameSearch = input("Now enter another player's full name: ")
 
     with open('Seasons_Stats.csv') as csvFile:
 
@@ -27,20 +53,20 @@ def createPlayerObject(firstUserNameSearch, secondUserNameSearch):
 
             if (firstUserNameSearch.lower() == f'{row[2]}'.lower()) or (secondUserNameSearch.lower() == f'{row[2]}'.lower()):
 
-                statsArray.append([(float(f'{row[52]}')/float(f'{row[6]}')), (float(f'{row[47]}')/float(f'{row[6]}')), (float(f'{row[46]}')/float(f'{row[6]}')), float(f'{row[36]}'), float(f'{row[39]}')])
-                playersNamesArray.append(f'{row[2]}')
-
+                createPlayerObjects(row)
                 lineCount += 1
+
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(statsArray, playersNamesArray)
+
+    print(clf.predict([[28, 10, 10, 0.4, 0.3]]))
+    print(clf.predict([[4, 0, 6, 0.2, 0.5]]))
+
+    opening()
+
+# Upon opening the program...
 
 statsArray = []
 playersNamesArray = []
 
-firstUserNameSearch = input("Enter a player's name: ")
-secondUserNameSearch = input("Now enter another player's name: ")
-createPlayerObject(firstUserNameSearch, secondUserNameSearch)
-
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(statsArray, playersNamesArray)
-
-print(clf.predict([[28, 10, 10, 0.4, 0.3]]))
-print(clf.predict([[4, 0, 6, 0.2, 0.5]]))
+opening()
