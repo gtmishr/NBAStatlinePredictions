17f99f85ae00d8079a9cc4d8d6645a82d1d86e84
Gautam Mishra <gautami.mishra@outlook.com>
2019-06-19 20:25:08 +1000

    Added some functionality documentation
    I need to add more actual explaining of the code in comments though. Coming soon hopefully.

--- seasonStatPredictor.py
+++ seasonStatPredictor.py
@@ -3,6 +3,9 @@ from sklearn import tree
 from sklearn.tree.export import export_text
 
 def opening():
+# Function ran upon initial running of the program, and after each program is fully executed.
+
+# "List Stats", "List", or "L" to access the listPlayerStats function, "2 Player Analysis", or "2P" to access the twoPlayerPrediction function, "Position Analysis" or "PA" to access the positionAnalysis function, and "Exit", or "E" to exit the program.
 
     statsArray.clear()
     playersNamesArray.clear()
@@ -10,7 +13,7 @@ def opening():
 
     initialInput = input("Welcome! What would you like to do? ")
 
-    if (initialInput.lower() == "List Stats".lower()) or (initialInput.lower() == "L".lower()) or (initialInput.lower() == "list".lower()):
+    if (initialInput.lower() == "List Stats".lower()) or (initialInput.lower() == "L".lower()) or (initialInput.lower() == "List".lower()):
         listPlayerStats()
     elif (initialInput.lower() == "2 Player Analysis".lower()) or (initialInput.lower() == "2P".lower()):
         twoPlayerPrediction()
@@ -21,12 +24,14 @@ def opening():
         quit()
 
 def createPlayerObjectsFiveStat(row):
-    # Points scored / games, assists / games, rebounds / games, three point percentage, two point percentage
+# Uses just points scored / games, assists / games, rebounds / games, three point percentage, two point percentage and is linked to an according array containing the name of the player(s).
 
     statsArray.append([(float(f'{row[52]}')/float(f'{row[6]}')), (float(f'{row[47]}')/float(f'{row[6]}')), (float(f'{row[46]}')/float(f'{row[6]}')), float(f'{row[36]}'), float(f'{row[39]}')])
     playersNamesArray.append(f'{row[2]}')
 
 def createPlayerObjectsAllStats(row):
+# CURRENTLY DOESN'T WORK
+# Uses every statistic that doesn't contain a letter / twoPlayerPrediction, and is linked to an according array containing the position of a certain player.
 
     tempArray = []
     x = 0
@@ -39,6 +44,8 @@ def createPlayerObjectsAllStats(row):
         x += 1
 
 def listPlayerStats():
+# CURRENTLY DOESN'T WORK
+# Goes through each player and lists their stats in a somewhat easy to read format.
 
     with open('Seasons_Stats.csv') as csvFile:
 
@@ -51,12 +58,10 @@ def listPlayerStats():
 
                 createPlayerObjectsFiveStat(row)
 
-    print(statsArray)
-    print(playersNamesArray)
-
     opening()
 
 def twoPlayerPrediction():
+# User inputs two names, and their statlines are collected from the createPlayerObjectsFiveStat function. Then, a decision tree is created between the names of the players and a statline is given for which it is predicted which of the two players would perform that statline.
 
     firstUserNameSearch = input("Enter a player's full name: ")
     secondUserNameSearch = input("Now enter another player's full name: ")
@@ -85,6 +90,8 @@ def twoPlayerPrediction():
     opening()
 
 def positionAnalysis():
+# CURRENTLY DOESN'T WORK
+# Gets the statistics from createPlayerObjectsAllStats function and uses it's correlation with their positions to find the barriers to each position.
 
     with open('Seasons_Stats.csv') as csvFile:
 
@@ -100,6 +107,8 @@ def positionAnalysis():
 
         r = export_text(clf, ["year", "position", "age", "games played", "minutes played", "per", "ts%", "3par", "ftr", "orb%", "drb%", "trb%", "ast%", "stl%", "blk%", "tov%", "usg%", "ows", "dws", "ws", "ws/48", "obpm", "dbpm", "bpm", "vorp", "fg", "fga", "fg%", "3p", "3pa", "3p%", "2p", "2pa", "2p%", "efg%", "ft", "fta", "ft%"])
         print(r)
+
+
 
         opening()
 
