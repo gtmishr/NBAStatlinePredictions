4bcea348ade18235e5cc058ea79f4c1bc523a9db
Gautam Mishra <gautami.mishra@outlook.com>
2019-06-19 20:14:34 +1000

    Tried to implement a positional analysis
    Tried to implement a system that would take in a statline and guess what position it would be made by, but it doesn't work (and I think it might be slightly beyond me to fix haha) but it would work REALLY well if it did I reckon.

--- seasonStatPredictor.py
+++ seasonStatPredictor.py
@@ -6,6 +6,7 @@ def opening():
 
     statsArray.clear()
     playersNamesArray.clear()
+    positionArray.clear()
 
     initialInput = input("Welcome! What would you like to do? ")
 
@@ -13,14 +14,29 @@ def opening():
         listPlayerStats()
     elif (initialInput.lower() == "2 Player Analysis".lower()) or (initialInput.lower() == "2P".lower()):
         twoPlayerPrediction()
+    elif (initialInput.lower() == "Position Analysis".lower()) or (initialInput.lower() == "PA".lower()):
+        positionAnalysis()
     elif (initialInput.lower() == "Exit".lower()) or (initialInput.lower() == "E".lower()):
         print("Thanks for checking me out!")
         quit()
 
-# Points scored / games, assists / games, rebounds / games, three point percentage, two point percentage
-def createPlayerObjects(row):
+def createPlayerObjectsFiveStat(row):
+    # Points scored / games, assists / games, rebounds / games, three point percentage, two point percentage
+
     statsArray.append([(float(f'{row[52]}')/float(f'{row[6]}')), (float(f'{row[47]}')/float(f'{row[6]}')), (float(f'{row[46]}')/float(f'{row[6]}')), float(f'{row[36]}'), float(f'{row[39]}')])
     playersNamesArray.append(f'{row[2]}')
+
+def createPlayerObjectsAllStats(row):
+
+    tempArray = []
+    x = 0
+
+    while x < 43:
+        if f'{row[0]}' != '' and f'{row[x]}' != '' and (x != 0) and (x != 2) and (x != 3) and (x != 5) and (x != 7) and (x != 21) and (x != 26) and  (int(f'{row[x]}') >= 1980):
+            tempArray.append(f'{row[x]}')
+        playersNamesArray.append(f'{row[3]}')
+        statsArray.append(tempArray)
+        x += 1
 
 def listPlayerStats():
 
@@ -33,7 +49,7 @@ def listPlayerStats():
 
             if (row[0] != "") and (f'{row[46]}' != ""):
 
-                createPlayerObjects(row)
+                createPlayerObjectsFiveStat(row)
 
     print(statsArray)
     print(playersNamesArray)
@@ -54,7 +70,7 @@ def twoPlayerPrediction():
 
             if (firstUserNameSearch.lower() == f'{row[2]}'.lower()) or (secondUserNameSearch.lower() == f'{row[2]}'.lower()):
 
-                createPlayerObjects(row)
+                createPlayerObjectsFiveStat(row)
                 aLineCount += 1
 
     clf = tree.DecisionTreeClassifier()
@@ -64,13 +80,33 @@ def twoPlayerPrediction():
     print(r)
 
     print(clf.predict([[30, 6, 9, 0.4, 0.3]]))
-    print(clf.predict([[4, 0, 6, 0.2, 0.2]]))
+    print(clf.predict([[0, 0, 0, 0, 0]]))
 
     opening()
+
+def positionAnalysis():
+
+    with open('Seasons_Stats.csv') as csvFile:
+
+        csvReader = csv.reader(csvFile, delimiter = ',')
+        pLineCount = 0
+
+        for row in csvReader:
+            createPlayerObjectsAllStats(row)
+            pLineCount += 1
+
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(statsArray, positionArray)
+
+        r = export_text(clf, ["year", "position", "age", "games played", "minutes played", "per", "ts%", "3par", "ftr", "orb%", "drb%", "trb%", "ast%", "stl%", "blk%", "tov%", "usg%", "ows", "dws", "ws", "ws/48", "obpm", "dbpm", "bpm", "vorp", "fg", "fga", "fg%", "3p", "3pa", "3p%", "2p", "2pa", "2p%", "efg%", "ft", "fta", "ft%"])
+        print(r)
+
+        opening()
 
 # Upon opening the program...
 
 statsArray = []
 playersNamesArray = []
+positionArray = []
 
 opening()
